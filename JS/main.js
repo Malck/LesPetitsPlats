b6ae6a90ee9f4464c59c71bb0c52e3fd447038ba
MC <Malckira@gmail.com>
2022-01-12 12:03:37 +0100

    usefull mainjs

--- JS/main.js
+++ JS/main.js
@@ -2,21 +2,6 @@ import {recipes} from "../JS/dataRecipes.js";
 
 import { displayRecipes } from "./displayRecipes.js";
 
-console.log(recipes);
 const recipesToDisplay = new displayRecipes();
 
 recipesToDisplay.addRecipeToMainPage(recipes);
-
-
-
-
-
- /* async function GetDataRecipes() {
-    const url = 'dataRecipes.js';
-    const response = await fetch(url) ; 
-    const data = await response.json();
-    //displayRecipes(data)
-    console.log(data);
-}
-
-GetDataRecipes()   */
